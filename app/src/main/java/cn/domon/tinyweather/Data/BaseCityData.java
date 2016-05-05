6ac5df1e5e566bc0767ff08833fd0f8a765999f8
Domon <viplizhao@gmail.com>
2016-05-05 23:21:56 +0800

    Add Some Data

---
+++ app/src/main/java/cn/domon/tinyweather/Data/BaseCityData.java
@@ -0,0 +1,52 @@
+package cn.domon.tinyweather.Data;
+
+/**
+ * Created by Domon on 16-5-5.
+ */
+public class BaseCityData {
+    private String city;
+    private String id;
+    private String cnty;
+    private String lat;
+    private String lon;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCnty() {
+        return cnty;
+    }
+
+    public void setCnty(String cnty) {
+        this.cnty = cnty;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+}
---
+++ app/src/main/java/cn/domon/tinyweather/Data/SuggestionData.java
@@ -0,0 +1,252 @@
+package cn.domon.tinyweather.Data;
+
+/**
+ * Created by Domon on 16-5-5.
+ */
+public class SuggestionData {
+    /**
+     * brf : 舒适
+     * txt : 白天不太热也不太冷，风力不大，相信您在这样的天气条件下，应会感到比较清爽和舒适。
+     */
+
+    private ComfBean comf;
+    /**
+     * brf : 较不宜
+     * txt : 较不宜洗车，未来一天无雨，风力较大，如果执意擦洗汽车，要做好蒙上污垢的心理准备。
+     */
+
+    private CwBean cw;
+    /**
+     * brf : 舒适
+     * txt : 建议着长袖T恤、衬衫加单裤等服装。年老体弱者宜着针织长袖衬衫、马甲和长裤。
+     */
+
+    private DrsgBean drsg;
+    /**
+     * brf : 较易发
+     * txt : 风较大，阴冷潮湿，较易发生感冒，体质较弱的朋友请注意适当防护。
+     */
+
+    private FluBean flu;
+    /**
+     * brf : 较适宜
+     * txt : 天气较好，但风力较大，推荐您进行室内运动，若在户外运动请注意防风��
+     */
+
+    private SportBean sport;
+    /**
+     * brf : 适宜
+     * txt : 天气较好，风稍大，但温度适宜，是个好天气哦。适宜旅游，您可以尽情地享受大自然的无限风光。
+     */
+
+    private TravBean trav;
+    /**
+     * brf : 强
+     * txt : 紫外线辐射强，建议涂擦SPF20左右、PA++的防晒护肤品。避免在10点至14点暴露于日光下。
+     */
+
+    private UvBean uv;
+
+    public ComfBean getComf() {
+        return comf;
+    }
+
+    public void setComf(ComfBean comf) {
+        this.comf = comf;
+    }
+
+    public CwBean getCw() {
+        return cw;
+    }
+
+    public void setCw(CwBean cw) {
+        this.cw = cw;
+    }
+
+    public DrsgBean getDrsg() {
+        return drsg;
+    }
+
+    public void setDrsg(DrsgBean drsg) {
+        this.drsg = drsg;
+    }
+
+    public FluBean getFlu() {
+        return flu;
+    }
+
+    public void setFlu(FluBean flu) {
+        this.flu = flu;
+    }
+
+    public SportBean getSport() {
+        return sport;
+    }
+
+    public void setSport(SportBean sport) {
+        this.sport = sport;
+    }
+
+    public TravBean getTrav() {
+        return trav;
+    }
+
+    public void setTrav(TravBean trav) {
+        this.trav = trav;
+    }
+
+    public UvBean getUv() {
+        return uv;
+    }
+
+    public void setUv(UvBean uv) {
+        this.uv = uv;
+    }
+
+    public static class ComfBean {
+        private String brf;
+        private String txt;
+
+        public String getBrf() {
+            return brf;
+        }
+
+        public void setBrf(String brf) {
+            this.brf = brf;
+        }
+
+        public String getTxt() {
+            return txt;
+        }
+
+        public void setTxt(String txt) {
+            this.txt = txt;
+        }
+    }
+
+    public static class CwBean {
+        private String brf;
+        private String txt;
+
+        public String getBrf() {
+            return brf;
+        }
+
+        public void setBrf(String brf) {
+            this.brf = brf;
+        }
+
+        public String getTxt() {
+            return txt;
+        }
+
+        public void setTxt(String txt) {
+            this.txt = txt;
+        }
+    }
+
+    public static class DrsgBean {
+        private String brf;
+        private String txt;
+
+        public String getBrf() {
+            return brf;
+        }
+
+        public void setBrf(String brf) {
+            this.brf = brf;
+        }
+
+        public String getTxt() {
+            return txt;
+        }
+
+        public void setTxt(String txt) {
+            this.txt = txt;
+        }
+    }
+
+    public static class FluBean {
+        private String brf;
+        private String txt;
+
+        public String getBrf() {
+            return brf;
+        }
+
+        public void setBrf(String brf) {
+            this.brf = brf;
+        }
+
+        public String getTxt() {
+            return txt;
+        }
+
+        public void setTxt(String txt) {
+            this.txt = txt;
+        }
+    }
+
+    public static class SportBean {
+        private String brf;
+        private String txt;
+
+        public String getBrf() {
+            return brf;
+        }
+
+        public void setBrf(String brf) {
+            this.brf = brf;
+        }
+
+        public String getTxt() {
+            return txt;
+        }
+
+        public void setTxt(String txt) {
+            this.txt = txt;
+        }
+    }
+
+    public static class TravBean {
+        private String brf;
+        private String txt;
+
+        public String getBrf() {
+            return brf;
+        }
+
+        public void setBrf(String brf) {
+            this.brf = brf;
+        }
+
+        public String getTxt() {
+            return txt;
+        }
+
+        public void setTxt(String txt) {
+            this.txt = txt;
+        }
+    }
+
+    public static class UvBean {
+        private String brf;
+        private String txt;
+
+        public String getBrf() {
+            return brf;
+        }
+
+        public void setBrf(String brf) {
+            this.brf = brf;
+        }
+
+        public String getTxt() {
+            return txt;
+        }
+
+        public void setTxt(String txt) {
+            this.txt = txt;
+        }
+    }
+}
