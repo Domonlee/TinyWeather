f302e6dbb7463194d6a1ddea51bd33ff24ace797
Domon <viplizhao@gmail.com>
2016-05-15 22:06:01 +0800

    Add NetUtil

---
+++ app/src/main/java/cn/domon/tinyweather/Utils/NetUtil.java
@@ -0,0 +1,60 @@
+package cn.domon.tinyweather.Utils;
+
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+/**
+ * Created by Domon on 16-5-15.
+ */
+public class NetUtil {
+
+    /**
+     * get IP , When is wifi
+     */
+
+    public static String getWifiIpAddress(Context context) {
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        if (!wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(true);
+        }
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        int ipAddress = wifiInfo.getIpAddress();
+        String ip = formatIpAddress(ipAddress);
+        return ip;
+    }
+
+    private static String formatIpAddress(int ipAddress) {
+        return (ipAddress & 0xFF) + "." +
+                ((ipAddress >> 8) & 0xFF) + "." +
+                ((ipAddress >> 16) & 0xFF) + "." +
+                (ipAddress >> 24 & 0xFF);
+    }
+
+    public static String getGPRSIpAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+                 en.hasMoreElements(); ) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumeration = intf.getInetAddresses();
+                     enumeration.hasMoreElements(); ) {
+                    InetAddress inetAddress = enumeration.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        return inetAddress.getHostAddress().toString();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+}
